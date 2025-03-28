94a471c35737329fa1792ea4b6409ba61aa0714e
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2025-03-28 21:44:59 +0400

    feat: Rust development environment with rust-toolchain and cargo support

---
+++ flake.nix
@@ -0,0 +1,62 @@
+{
+  description = "Development environment configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      ...
+    }@inputs:
+    let
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      forEachSystem = f: nixpkgs.lib.genAttrs systems f;
+    in
+    {
+      formatter = forEachSystem (s: nixpkgs.legacyPackages.${s}.nixfmt-rfc-style);
+
+      devShells = forEachSystem (
+        s:
+        with nixpkgs.legacyPackages.${s};
+        let
+          overrides = (builtins.fromTOML (builtins.readFile ./rust-toolchain.toml));
+        in
+        {
+          default = mkShell {
+            strictDeps = true;
+
+            nativeBuildInputs = [
+              rustup
+              rustPlatform.bindgenHook
+            ];
+
+            buildInputs = [
+              gcc
+              pkg-config
+              cargo
+              openssl
+              sqlite
+              rustfmt
+              rust-analyzer
+            ];
+
+            RUSTC_VERSION = overrides.toolchain.channel;
+
+            # https://github.com/rust-lang/rust-bindgen#environment-variables
+            shellHook = ''
+              export PATH="''${CARGO_HOME: -~/.cargo}/bin":"$PATH"
+              export PATH="''${RUSTUP_HOME: -~/.rustup}/toolchains/$RUSTC_VERSION-${stdenv.hostPlatform.rust.rustcTarget}/bin":"$PATH"
+            '';
+          };
+        }
+      );
+    };
+}
